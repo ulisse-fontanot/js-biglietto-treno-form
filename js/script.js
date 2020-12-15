4433cce151e33090ef098a8d30c5d799d7a89f0b
ulisse-fontanot <ulissef99@gmail.com>
2020-12-15 17:57:57 +0100

    completo-miglioro css

--- js/script.js
+++ js/script.js
@@ -33,3 +33,42 @@ function() {
 
 }
 );
+
+// WARNING:
+//ANNULLA
+
+var element = document.getElementById("annulla");
+element.addEventListener('click',
+function() {
+
+  var nome = document.getElementById('nome').value;
+  console.log(nome);
+  var km = document.getElementById('km').value;
+  console.log(km);
+  var eta = document.getElementById('eta').value;
+  console.log(eta);
+
+  var costo = km * 0.21;
+  var message = 'Tariffa Standard';
+
+  if (eta == 'minorenne') {
+    costo -= costo * 0.2;
+    message = "Sconto minorenne";
+  } else if (eta == 'anziano') {
+    costo -= costo * 0.4;
+    message = "Sconto anziano";
+  }
+
+  document.getElementById('nominativo').innerHTML = "";
+
+  document.getElementById('offerta').innerHTML = "";
+  var carrozza = Math.floor(Math.random()*9) +1;
+  document.getElementById('carrozza').innerHTML = "";
+  var min = 80000;
+  var max = 100000;
+  var codice = Math.floor(Math.random() * (max - min + 1) ) + min;
+  document.getElementById('codice').innerHTML = "";
+  document.getElementById('costo').innerHTML = "";
+
+}
+);
